b3230f745ca3ae9126bd91184ce3df36dd66bd25
July Tikhonov <july.tikh@gmail.com>
2017-05-05 22:43:47 +0300

    add some more sources

--- .jeolm/local.py
+++ .jeolm/local.py
@@ -1,10 +1,1 @@
-from jeolm.driver.groups import GroupsDriver
-from jeolm.driver.authors import AuthorsDriver
-from jeolm.driver.addtoc import AddToCDriver
-from jeolm.driver.source_link import SourceLinkDriver
-
-class Driver(GroupsDriver, AuthorsDriver, AddToCDriver, SourceLinkDriver):
-
-    def _select_outname(self, target, metarecord, date=None):
-        return super()._select_outname(target, metarecord, date=None)
-
+from jeolm.driver.training import TrainingDriver as Driver
